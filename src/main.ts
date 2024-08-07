36688f6245fe8f539058f40e34216468f3b3502c
Jhonata-vss <jhonatasenac15@gmail.com>
2024-08-07 11:52:22 -0300

    Render

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  process.env.TZ = '-03:00'; // Configuramos o fuso-horário
+  const config = new DocumentBuilder()
+  .setTitle('Blog Pessoal')
+  .setDescription('Projeto Blog Pessoal')
+  .setContact("Jhonata Venicius","https://github.com/Jhonata-vss","jhonatasenac15@gmail.com")
+  .setVersion('1.0')
+  .addBearerAuth()
+  .build();
 
-  app.useGlobalPipes(new ValidationPipe()); // Habilitamos o Validation Globalmente
+  const document = SwaggerModule.createDocument(app,config);
+  SwaggerModule.setup('/swagger',app,document);
 
-  app.enableCors(); // Habilitamos requisições de outras origens (Servidores)
+  process.env.TZ = '03:00'; // Configurar o TZ para não dar erro ao trabalharmos com datas
 
-  await app.listen(4000);
+  app.useGlobalPipes(new ValidationPipe()); // Habilitando o Validation para fazer a validação dos dados (@NotNull,etc)
+  app.enableCors();
+  await app.listen(process.env.PORT || 4000);
 }
+
 bootstrap();
